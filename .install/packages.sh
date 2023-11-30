61b212084a7659cffc7a326096f45a815ff625d3
doki <h18673628658@outlook.com>
2023-11-30 13:37:54 +0800

    add packages

--- .install/packages.sh
+++ .install/packages.sh
@@ -5,9 +5,80 @@ packagesPacman=(
     "ttf-fira-code" 
     "ttf-firacode-nerd" 
     "fish"
+    "eza"
+
+    "sof-firmware" 
+    "alsa-firmware" 
+    "alsa-ucm-conf"
+    "alsa-utils"
+    "pulseaudio"
+
+    "ntfs-3g"
+    timeshift
+
+    "adobe-source-han-serif-cn-fonts" 
+    "wqy-zenhei"
+    "noto-fonts" 
+    "noto-fonts-cjk" 
+    "noto-fonts-emoji" 
+    "noto-fonts-extra"
+
+    "ark"
+    "p7zip"
+    "gwenview"
+    virtualbox
+
+    "vlc" 
+    "eza" 
+    "figlet" 
+    "mpv" 
+
+    "fcitx5-im"
+    fcitx5-chinese-addons
+    fcitx5-anthy
+    fcitx5-pinyin-moegirl
+    fcitx5-material-color
+
+    blueman
+
+    mesa 
+    lib32-mesa 
+    xf86-video-amdgpu 
+    vulkan-radeon 
+    lib32-vulkan-radeon
+
+    v2ray 
+    v2raya
+
+    okular
+    telegram-desktop
+    linuxqq
+    thunderbird
+    lutris
+    zerotier-one
+    nutstore-experimental
+
+    # wine
+    wine wine-mono 
+    wine_gecko 
+    wine-staging 
+    giflib 
+    lib32-giflib 
+    libpng 
+    lib32-libpng 
+    libldap 
+    lib32-libldap 
+    gnutls 
+    lib32-gnutls
+    mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error 
+    lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo 
+    sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama 
+    ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 
+    lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 );
 
 packagesYay=(
     "trizen"
     "neofetch" 
+    google-chrome
 );
