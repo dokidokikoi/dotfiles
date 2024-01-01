3a3fc8749d7965e34cc6a3b3e8ee7f3950c6ac7a
doki <haruakze_doki@163.com>
2024-01-01 20:39:12 +0800

    edit

--- .install/packages.sh
+++ .install/packages.sh
@@ -1,67 +1,67 @@
 packagesPacman=(
-    "neovim" 
-    "ttf-font-awesome" 
-    "ttf-fira-sans" 
-    "ttf-fira-code" 
-    "ttf-firacode-nerd" 
-    "fish"
-    "eza"
+	neovim
+	ttf-font-awesome
+	ttf-fira-sans
+	ttf-fira-code
+	ttf-firacode-nerd
+	fish
+	eza
 
-    "sof-firmware" 
-    "alsa-firmware" 
-    "alsa-ucm-conf"
-    "alsa-utils"
-    "pulseaudio"
+	sof-firmware
+	alsa-firmware
+	alsa-ucm-conf
+	alsa-utils
+	pulseaudio
 
-    "ntfs-3g"
-    timeshift
+	ntfs-3g
+	timeshift
 
-    "adobe-source-han-serif-cn-fonts" 
-    "wqy-zenhei"
-    "noto-fonts" 
-    "noto-fonts-cjk" 
-    "noto-fonts-emoji" 
-    "noto-fonts-extra"
-    "ttf-jetbrains-mono"
+	adobe-source-han-serif-cn-fonts
+	wqy-zenhei
+	noto-fonts
+	noto-fonts-cjk
+	noto-fonts-emoji
+	noto-fonts-extra
+	ttf-jetbrains-mono
 
-    "ark"
-    "p7zip"
-    "gwenview"
-    virtualbox
+	ark
+	p7zip
+	gwenview
+	virtualbox
 
-    "vlc" 
-    "eza" 
-    "figlet" 
-    "mpv" 
+	vlc
+	eza
+	figlet
+	mpv
 
-    "fcitx5-im"
-    fcitx5-chinese-addons
-    fcitx5-anthy
-    fcitx5-pinyin-moegirl
-    fcitx5-material-color
+	fcitx5-im
+	fcitx5-chinese-addons
+	fcitx5-anthy
+	fcitx5-pinyin-moegirl
+	fcitx5-material-color
 
-    blueman
+	blueman
 
-    mesa 
-    lib32-mesa 
-    xf86-video-amdgpu 
-    vulkan-radeon 
-    lib32-vulkan-radeon
+	mesa
+	lib32-mesa
+	xf86-video-amdgpu
+	vulkan-radeon
+	lib32-vulkan-radeon
 
-    v2ray 
-    v2raya
+	v2ray
+	v2raya
 
-    okular
-    telegram-desktop
-    thunderbird
-    lutris
-    zerotier-one
-);
+	okular
+	telegram-desktop
+	thunderbird
+	lutris
+	zerotier-one
+)
 
 packagesYay=(
-    "trizen"
-    "neofetch" 
-    google-chrome
-    nutstore-experimental
-    linuxqq
-);
+	trizen
+	neofetch
+	google-chrome
+	nutstore-experimental
+	linuxqq
+)
